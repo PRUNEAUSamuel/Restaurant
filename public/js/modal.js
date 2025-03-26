a3a492360acfc3ee5202a5cca2cee4b1433d285a
Pruneau Samuel <pruneausamuel@gmail.com>
2025-03-26 14:58:54 +0100

    remis de base et fonctionnel avec lien

---
+++ public/js/modal.js
@@ -0,0 +1,15 @@
+const myModal = document.getElementById('authentication-modal');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    myModal.classList.remove('hidden');
+});
+
+document.getElementById('closeModal').addEventListener('click', function() {
+
+    myModal.classList.add('hidden');
+});
+
+console.log("yo")
